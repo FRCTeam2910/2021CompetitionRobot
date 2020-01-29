02a08d36cf6ccc2b23ef68764da2a01441ee1fe8
Bryce Howard <howardabryce@gmail.com>
2020-01-28 20:38:05 -0800

    Add dashboard tab for climber (#52)

--- src/main/java/org/frcteam2910/c2020/subsystems/ClimberSubsystem.java
+++ src/main/java/org/frcteam2910/c2020/subsystems/ClimberSubsystem.java
@@ -1,6 +1,9 @@
 package org.frcteam2910.c2020.subsystems;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import org.frcteam2910.c2020.Constants;
 import org.frcteam2910.common.robot.UpdateManager;
@@ -10,6 +13,20 @@ public class ClimberSubsystem implements Subsystem, UpdateManager.Updatable {
     private final Solenoid deploySolenoid = new Solenoid(Constants.CLIMBER_DEPLOY_SOLENOID_PORT);
     private final Solenoid extendSolenoid = new Solenoid(Constants.CLIMBER_EXTEND_SOLENOID_PORT);
 
+    private final NetworkTableEntry isDeployedEntry;
+    private final NetworkTableEntry isExtendedEntry;
+
+    public ClimberSubsystem() {
+        ShuffleboardTab tab = Shuffleboard.getTab("Climber");
+        isDeployedEntry = tab.add("Is Climber Deployed", false)
+                .withPosition(0, 0)
+                .withSize(0, 0)
+                .getEntry();
+        isExtendedEntry = tab.add("Is Climber Extended", false)
+                .withPosition(0, 1)
+                .withSize(0, 0)
+                .getEntry();
+    }
 
     public void deployClimber(){
         deploySolenoid.set(true);
@@ -32,4 +49,9 @@ public class ClimberSubsystem implements Subsystem, UpdateManager.Updatable {
 
     }
 
+    @Override
+    public void periodic() {
+        isExtendedEntry.setBoolean(isExtended());
+        isDeployedEntry.setBoolean(deploySolenoid.get());
+    }
 }
