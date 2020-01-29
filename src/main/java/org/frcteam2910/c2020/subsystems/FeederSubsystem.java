829926d85116f184e3edb836b284deb5bb902a17
Bryce Howard <howardabryce@gmail.com>
2020-01-28 20:52:42 -0800

    Add dashboard tab for feeder (#50)
    * Add Feeder Smartdashboard Prints
    
    * Change default entry types

--- src/main/java/org/frcteam2910/c2020/subsystems/FeederSubsystem.java
+++ src/main/java/org/frcteam2910/c2020/subsystems/FeederSubsystem.java
@@ -2,7 +2,10 @@ package org.frcteam2910.c2020.subsystems;
 
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import org.frcteam2910.c2020.Constants;
 import org.frcteam2910.common.robot.UpdateManager;
@@ -13,9 +16,27 @@ public class FeederSubsystem implements Subsystem, UpdateManager.Updatable {
     private DigitalInput fullSensor = new DigitalInput(Constants.FEEDER_IS_FULL_SENSOR_PORT);
     private DigitalInput intakeBallSensor = new DigitalInput(Constants.FEEDER_INTAKE_BALL_SENSOR_PORT);
 
+    private final NetworkTableEntry fullEntry;
+    private final NetworkTableEntry hasBallEntry;
+    private final NetworkTableEntry motorSpeedEntry;
+
+    public FeederSubsystem() {
+        ShuffleboardTab tab = Shuffleboard.getTab("Feeder");
+        fullEntry = tab.add("Is feeder full", false)
+                .withPosition(0, 0)
+                .withSize(1, 1)
+                .getEntry();
+        hasBallEntry = tab.add("Does shooter have ball", false)
+                .withPosition(0, 1)
+                .withSize(1, 1)
+                .getEntry();
+        motorSpeedEntry = tab.add("Motor Speed", 0.0)
+                .withPosition(1, 0)
+                .withSize(1, 1)
+                .getEntry();
+    }
 
     public void spinMotor(double speed){
-
         motor.set(TalonFXControlMode.PercentOutput,speed);
     }
 
@@ -37,4 +58,10 @@ public class FeederSubsystem implements Subsystem, UpdateManager.Updatable {
 
     }
 
+    @Override
+    public void periodic() {
+        fullEntry.setBoolean(isFull());
+        hasBallEntry.setBoolean(intakeBallSensor.get());
+        motorSpeedEntry.setDouble(motor.getSensorCollection().getIntegratedSensorVelocity());
+    }
 }
