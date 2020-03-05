88c981404507772f6505a8d4284192f3d26f39b6
Jacob Bublitz <jacob@bublitz.com>
2020-03-04 19:09:51 -0800

    Rezero Swerve Modules

--- src/main/java/org/frcteam2910/c2020/Constants.java
+++ src/main/java/org/frcteam2910/c2020/Constants.java
@@ -54,10 +54,10 @@ public class Constants {
     public static final int FEEDER_IS_FULL_SENSOR_PORT = 0;
     public static final int FEEDER_INTAKE_BALL_SENSOR_PORT = 1;
 
-    private static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET_COMPETITION = -Math.toRadians(61.5);
-    private static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET_COMPETITION = -Math.toRadians(51.4);
-    private static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET_COMPETITION = -Math.toRadians(77.5);
-    private static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET_COMPETITION = -Math.toRadians(202.1);
+    private static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET_COMPETITION = -Math.toRadians(63.2);
+    private static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET_COMPETITION = -Math.toRadians(52.4);
+    private static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET_COMPETITION = -Math.toRadians(353.15);
+    private static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET_COMPETITION = -Math.toRadians(201.1);
     private static final double SHOOTER_HOOD_MIN_ANGLE_COMPETITION = Math.toRadians(26.90);
     private static final double SHOOTER_HOOD_MAX_ANGLE_COMPETITION = Math.toRadians(64.54);
     private static final double SHOOTER_HOOD_OFFSET_COMPETITION = Math.toRadians(55.91) + SHOOTER_HOOD_MAX_ANGLE_COMPETITION;
